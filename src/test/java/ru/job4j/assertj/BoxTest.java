c31188a1ace989c79b5b66c5d583120a9f87ce3e
Mariya <mma@intelins.ru>
2023-11-28 16:06:48 +0300

    Box

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -11,4 +11,53 @@ class BoxTest {
         String name = box.whatsThis();
         assertThat(name).isEqualTo("Sphere");
     }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void getNumVert() {
+        Box box = new Box(4, 10);
+        int numOfVert = box.getNumberOfVertices();
+        assertThat(numOfVert).isEqualTo(4);
+    }
+
+    @Test
+    void getNumVert2() {
+        Box box = new Box(0, 10);
+        int numOfVert = box.getNumberOfVertices();
+        assertThat(numOfVert).isEqualTo(0);
+    }
+
+    @Test
+    void isExists() {
+        Box box = new Box(4, 10);
+        boolean exists = box.isExist();
+        assertThat(exists).isEqualTo(true);
+    }
+
+    @Test
+    void isExists2() {
+        Box box = new Box(0, 10);
+        boolean exists = box.isExist();
+        assertThat(exists).isEqualTo(true);
+    }
+
+    @Test
+    void getarea() {
+        Box box = new Box(4, 10);
+        double gerAr = box.getArea();
+        assertThat(gerAr).isEqualTo(Math.sqrt(3) * 100);
+    }
+
+    @Test
+    void getarea2() {
+        Box box = new Box(0, 10);
+        double gerAr = box.getArea();
+        assertThat(gerAr).isEqualTo(4 * Math.PI * 100);
+    }
 }
