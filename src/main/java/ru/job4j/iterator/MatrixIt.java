645860421bbc5878a1350d0c60fcc42c1396a2f0
Mariya <mma@intelins.ru>
2023-12-05 13:49:33 +0300

    MatrixIt

---
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -0,0 +1,36 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row;
+    private int column;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && data[row].length == 0) {
+            row++;
+        }
+        return row < data.length && column < data[row].length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int res = data[row][column++];
+        System.out.println(res);
+        if (column == data[row].length) {
+            row++;
+            column = 0;
+        }
+        return res;
+    }
+}
