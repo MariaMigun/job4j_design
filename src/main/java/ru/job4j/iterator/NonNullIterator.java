7761ef8cc65e793d05a2aabf8bb9e0461dbb8fa4
Mariya <mma@intelins.ru>
2023-12-25 13:47:14 +0300

    NonNullIterator

---
+++ src/main/java/ru/job4j/iterator/NonNullIterator.java
@@ -0,0 +1,40 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NonNullIterator implements Iterator<Integer> {
+
+    private Integer[] data;
+    private int pos = -1;
+    private int nextPos = -1;
+
+    public NonNullIterator(Integer[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return pos != findNotNull();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        pos = nextPos;
+        return data[pos];
+    }
+
+    public Integer findNotNull() {
+        for (int i = pos + 1;i < data.length; i++) {
+            if (data[i] != null) {
+                nextPos = i;
+                break;
+            }
+        }
+        return nextPos;
+    }
+
+}
