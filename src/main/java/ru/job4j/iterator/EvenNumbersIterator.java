80a317c8fae526a90ade821017789b5ce7e47d34
Mariya <mma@intelins.ru>
2023-12-25 13:26:32 +0300

    EvenNumbersIterator

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -7,6 +7,8 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     private int[] data;
     private int ind;
+    private int pos = -1;
+    private int nextPos = -1;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
@@ -14,19 +16,25 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 /*iterator.next() - возвращают только четные числа. В этом примере - это 4 и 2.*/
     @Override
     public boolean hasNext() {
-        return ind < data.length;/* && data[ind]%2 == 0;*/
+        return pos != findEven();
     }
 
     @Override
     public Integer next() {
-        int res;
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        if  (data[ind]%2 == 0) {
-            res = data[ind];
+        pos = nextPos;
+        return data[pos];
+    }
+
+    public Integer findEven() {
+        for (int i = pos + 1;i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                nextPos = i;
+                break;
+            }
         }
-        ind++;
-        return res;
+        return nextPos;
     }
 }
