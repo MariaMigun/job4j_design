40b2139084f06594891661f23065dc8fdb43b15a
Mariya <mma@intelins.ru>
2023-12-25 17:20:12 +0300

    CyclicIterator

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,34 @@
+package ru.job4j.iterator;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+
+    private List<T> data;
+    private Iterator itr;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+        itr = data.iterator();
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return itr.hasNext();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            return data.get(1);
+        /*    throw new NoSuchElementException();*/
+        }
+        else {
+            return data.iterator().next();
+        }
+    }
+}
